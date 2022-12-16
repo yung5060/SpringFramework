7819b3c33fae95fbf2bc0bed40f24edf49d9ea79
Yung Cho <yung5060@kakao.com>
2022-12-17 02:32:57 +0900

    first commit

---
+++ src/main/java/demo/Doctor.java
@@ -0,0 +1,22 @@
+package demo;
+
+public class Doctor implements Staff{
+
+private String qualification;
+
+    public Doctor(String qualification) {
+        this.qualification = qualification;
+    }
+
+    public String getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+
+    public void assist() {
+        System.out.println("Doctor is assisting");
+    }
+}
